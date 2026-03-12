dee2718203ff82675778898f341e3dc198e6bf54
Prashant <thedevopsprashant@gmail.com>
2026-03-12 12:36:48 +0530

    added tfvars

---
+++ Jenkins-Server-TF/terraform.tfvars
@@ -0,0 +1,9 @@
+vpc-name      = "vpc-jenkins-default"   #"Jenkins-vpc" 
+igw-name      = "igw-jenkins-default"   #"Jenkins-igw" 
+subnet-name   = "public-jenkins-1"       #"Jenkins-subnet" 
+rt-name       = "public_rt-jenkins-default" #"Jenkins-route-table"
+sg-name       = "Jenkins-sg"
+instance-name = "Jenkins-server"
+key-name      = "Jenkins-key-pair"
+iam-role      = "Jenkins-iam-role"
+
--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -9,12 +9,14 @@ java --version
 
 # Installing Jenkins
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
 
 # Installing Docker
 sudo apt update
